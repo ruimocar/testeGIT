de81def32d31d633fe4be3f59de4a7cdf9093690
Rui Carrasqueira <rmcarrasqueira@gmail.com>
2020-11-09 12:26:45 +0000

    Changed message

--- testeGIT/testeGIT.cpp
+++ testeGIT/testeGIT.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\nThis is a test to Store files in GIT\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
